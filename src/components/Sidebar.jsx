ccd0a71adef640fcc3a08fd1a8738f2a6b6a88ad
Prateek Rath <prateekrath28@gmail.com>
2022-08-29 22:08:18 +0530

    render sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,26 +2,30 @@ import React from 'react'
 import {  Stack } from '@mui/material'
 import { categories } from '../utils/constants'
 
-const selectedCategory='New'
 
-const Sidebar = () => {
+
+const Sidebar = ({selectedCategory,setSelectedCategory,name,setName}) => {
   return (
       <Stack direction="row" sx={{
           overflowY: 'auto',
           height: { sx: 'auto', md: '95%' },
           flexDirection:{md: 'column'}
       }}>
-          {categories.map((category) => (
+          {categories.map((category,i) => (
               <button className='category-btn'
-                  style={{ background: category.name === selectedCategory && '#fc1503', color: 'white' }}
-                  key={category.name}
+                  onClick={() => {
+                      setSelectedCategory(category.actual)
+                      setName(category.name)
+                  }}
+                  style={{ background: category.actual === selectedCategory && '#fc1503', color: 'white' }}
+                  key={i}
               >
                   <span style={{
-                      color: category.name === selectedCategory ? 'white' : 'red',
+                      color: category.actual === selectedCategory ? 'white' : 'red',
                       marginRight:'15px'
                   }}>{category.icon}</span>
                   <span style={{
-                      opacity:category.name===selectedCategory ? '1':'0.8'
+                      opacity:category.actual===selectedCategory ? '1':'0.8'
                   }}>{category.name}</span>
               </button>
           ))}
