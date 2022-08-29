c5cb2290c23d9ad27412bf3545a3d70b0bf7d1b7
Prateek Rath <prateekrath28@gmail.com>
2022-08-29 22:08:39 +0530

    render channel and video

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { Typography,Card,CardContent,CardMedia } from '@mui/material'
+import {CheckCircle} from '@mui/icons-material'
+import { demoThumbnailUrl,demoVideoUrl,demoVideoTitle,demoChannelUrl,demoChannelTitle } from '../utils/constants'
+
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
+    
+  return (
+      <Card sx={{width:{md:'320px',xs:'100%'},boxShadow:'none',borderRadius:'none'}}>
+          <Link to={videoId ? `/video/${videoId}`: demoVideoUrl}>
+              <CardMedia image={snippet?.thumbnails?.high?.url}
+                  alt={snippet?.title}
+                  sx={{width:'358px',height:'180px'}}
+              />
+          </Link>
+          <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+              <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                  <Typography variant='subtitle1' fontWeight="bold" color='#fff'>
+                      {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
+                  </Typography>
+              </Link>
+              <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+                  <Typography variant='subtitle2' fontWeight="bold" color='gray'>
+                      {snippet?.channelTitle || demoChannelTitle}
+                      <CheckCircle sx={{fontSize:12,color:'gray', ml:'5px'}} />
+                  </Typography>
+              </Link>
+          </CardContent>
+    </Card>
+  )
+}
+
+export default VideoCard
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
 
-const Videos = () => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return "Loading...";
+//   console.log(videos);
+  let channel;
+  videos.forEach((vid) => {
+    if (vid?.id?.channelId) {
+      channel = vid;
+    }
+  });
   return (
-    <div>Videos</div>
-  )
-}
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      <ChannelCard channelDetail={channel} />
+      {videos.map((item, idx) => (
+        <Box key={idx}>{item.id.videoId && <VideoCard video={item} />}</Box>
+      ))}
+    </Stack>
+  );
+};
 
-export default Videos
+export default Videos;
