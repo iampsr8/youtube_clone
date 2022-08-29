81d6fb892cb69466427626ae03df3f4cf63e6896
Prateek Rath <prateekrath28@gmail.com>
2022-08-29 22:08:02 +0530

    render feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,21 +2,38 @@ import React,{useState,useEffect} from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 import Sidebar from './Sidebar'
 import Videos from './Videos'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+
 
 const Feed = () => {
+
+  const [name, setName] = useState('Technology')
+  const [selectedCategory, setSelectedCategory] = useState('apple mkbhd')
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data)=>{setVideos(data.items)})
+  },[selectedCategory])
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: 'row' } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        <Sidebar/>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          name={name}
+          setName={setName}
+        />
         <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
           &copy;YeahTube. 2022 Prateek Rath
         </Typography>
       </Box>
       <Box p={2} sx={{overflowY:'auto',height:'90vh', flex:2}}>
       <Typography variant='h4' fontWeight='bold' mb={2} sx={{color:'white'}}>
-        New <span style={{color:'#fc1503'}}>Videos</span>
+        {name} <span >Videos</span>
         </Typography>
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
